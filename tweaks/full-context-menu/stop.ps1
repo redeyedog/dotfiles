7691ad9345bc33a4b5c67da9a60c70bd0998c639
petrroots <b34.33d@gmail.com>
2024-02-28 07:59:58 +0400

    rm fcmenu

--- tweaks/full-context-menu/stop.ps1
+++
@@ -1,6 +0,0 @@
-# Скрипт для возвращения стандартного контекстного меню Windows 11
-
-$registryPath = "HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}"
-
-# Удаление ключа реестра
-Remove-Item -Path $registryPath -Recurse
